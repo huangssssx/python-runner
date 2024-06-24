2c6c34476dbacc5ccac2edb3933a8529ca25f813
黄传剑 <huangchuanjian@cestc.cn>
2024-06-24 12:59:04 +0800

    # 增加对python脚本的支持

---
+++ app/components/upload/index.tsx
@@ -0,0 +1,19 @@
+"use client";
+import axios from "axios";
+import React, { useEffect, useState } from 'react';
+
+export default function Upload() {
+    const [systemInfo, setSystemInfo] = useState({});
+    useEffect(() => {
+        axios.get('http://localhost:3000/api/run-python').then((res) => {
+            console.log(res);
+            setSystemInfo(res.data.system_info || {});
+        })
+    }, [])
+    return (
+        <div>
+            <h1 style={{ color: "red", fontSize: "36px" }}>python script back infos: </h1>
+            <pre>{JSON.stringify(systemInfo, null, 2)}</pre>
+        </div >
+    );
+}
--- app/pys/script.py
+++ app/pys/script.py
@@ -5,7 +5,15 @@ import json
 def main():
     data = {
         "message": "Hello from Python!",
-        "success": True
+        "success": True,
+        "system_info": {
+            "python_version": sys.version,
+            "version_info": sys.version_info
+        },
+        "environment_info": {
+            "os": sys.platform,
+            "version": sys.version_info.major
+        }
     }
     print(json.dumps(data))
 
